36f0cd11d5cd104c256d0795482185a6d2d36969
Alfredoes234 <114923333+Alfredoes234@users.noreply.github.com>
2022-11-15 15:57:03 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Binary Search
+
+def binarysearch():
+  # Repeat until the pointers low and high meat each other
+  while low <= high:
+    mid = low + (high - low)//2 
+    
+    if array[mid] == x:
+      return mid
+    elif array[mid] < x:
+      low = mid + 1
+    else:
+      high = mid - 1
+ return -1 
